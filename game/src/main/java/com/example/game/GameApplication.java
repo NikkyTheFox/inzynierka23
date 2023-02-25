a21142b590bb74f84fed96c85539eb304e7413ba
NikkyTheFox <adi.pochec@gmail.com>
2023-02-25 12:09:22 +0100

    all working base services (game, config, and eureka)

--- game/src/main/java/com/example/game/GameApplication.java
+++ game/src/main/java/com/example/game/GameApplication.java
@@ -1,5 +1,6 @@
 package com.example.game;
 
+
 import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @EnableEurekaClient
 @SpringBootApplication
---
+++ game/src/main/java/com/example/game/entities/Character.java
@@ -0,0 +1,24 @@
+package com.example.game.entities;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString
+@EqualsAndHashCode
+@Entity
+@Table(name="characters")
+public class Character {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Integer id;
+    private String name;
+
+}
